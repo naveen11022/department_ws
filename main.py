ec83fcef2759cf8b5e467d308b4ab1ebefc34e8a
naveen11022 <150673768+naveen11022@users.noreply.github.com>
2025-03-12 20:24:30 +0530

    backend

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
-from endpoints import achieve_endpoints, faculty_endpoint, Notes_endpoints, stu_endpoints, TT_endpoints
+from endpoints import achieve_endpoints, faculty_endpoint, Notes_endpoints, stu_endpoints, TT_endpoints, event, work
 from auth import router
 app = FastAPI()
 
@@ -11,6 +11,8 @@ app.include_router(faculty_endpoint.router)
 app.include_router(Notes_endpoints.router)
 app.include_router(stu_endpoints.router)
 app.include_router(TT_endpoints.router)
+app.include_router(event.router)
+app.include_router(work.router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
